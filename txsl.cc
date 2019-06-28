0872881883e8508fcafd5504ca0352260d8a2cca
Polaris1949 <Polaris1949@126.com>
2019-06-28 12:37:13 +0800

    Update txsl.cc

--- txsl.cc
+++ txsl.cc
@@ -10,13 +10,8 @@ namespace fs = std::filesystem;
 std::string help_msg =
 "Usage: %s [OPTION]... [STRING]\n"
 "Make convertion between text and string literal.\n\n"
-"Options:\n"
 "      -s         convert text to string literal (aka --mode=s)\n"
 "      -t         convert string literal to text (aka --mode=t)\n"
-"      -r         open response mode (only support -s)\n"
-"      -e         enable escape sequence (aka --escape=std)\n"
-"      -o         octal number for escape sequence (aka --number=oct)\n"
-"      -x         hexdecimal number for escape sequence (aka --number=hex)\n"
 "      -n         number only for escape sequence (aka --escape=number)\n"
 "      --mode={s|t} (default=s)\n"
 "                 select convertion mode\n"
@@ -24,6 +19,7 @@ std::string help_msg =
 "                 control escape sequence\n"
 "      --number={oct|hex} (default=hex)\n"
 "                 escape sequence number\n"
+"      --response enable response mode (only support -s)\n"
 "      --help     display this help and exit\n"
 "      --version  ouput version information and exit";
 std::string version_msg =
@@ -37,21 +33,21 @@ std::string version_msg =
 std::string exe_name;
 
 int optmode = 0; //0:default,1:s,2:t
-int optr = 0;
-int escape = 0; //0:default,1:never,2:std,3:strict,4:number
-int opte = 0;
-int optn = 0;
+int optresponse = 0;
+int optescape = 0; //0:default,1:never,2:std,3:strict,4:number
+int optnumber = 0;
 int opthelp = 0;
 int optversion = 0;
 std::string error_msg;
 
+bool parse_exe_name();
 bool parse_cmd(int, char**);
-int check_opt(const char*);
+bool check_opt(const char*);
 bool check_opt_value();
 bool parse_opt(const char*);
 bool check_opt_arg(const char*, int);
 bool parse_arg_mode(const char*);
-bool parse_exe_name();
+bool parse_arg_escape(const char*);
 void error_opt_bad(const char*);
 void error_opt_conflict(const char*, const char*);
 void error_opt_unrecognized(const char*);
@@ -72,21 +68,35 @@ bool parse_exe_name(const char* name)
     return true;
 }
 
-int check_opt(const char* opt)
+bool parse_cmd(int argc, char** argv)
+{
+    if (!parse_exe_name(argv[0]))
+        return false;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!check_opt(argv[i])) return false;
+        if (!parse_opt(argv[i])) return false;
+    }
+
+    return true;
+}
+
+bool check_opt(const char* opt)
 {
     if (opt[0] != '-')
     {
         error_opt_bad(opt);
-        return 0;
+        return false;
     }
     else
     {
         if (opt[1] == '-')
         {
-            if (std::islower(opt[2])) return 2;
-            else {error_opt_bad(opt); return 0;}
+            if (std::islower(opt[2])) return true;
+            else {error_opt_bad(opt); return false;}
         }
-        else return 1;
+        else return true;
     }
 }
 
@@ -94,20 +104,25 @@ bool parse_opt(const char* opt)
 {
     std::string topt{opt};
 
-    if (topt == "help")
+    if (topt == "--help")
     {
         opthelp = 1;
     }
-    else if (topt == "version")
+    else if (topt == "--version")
     {
         optversion = 1;
     }
-    else if (topt.substr(0, 4) == "mode")
+    else if (topt.substr(0, 6) == "--mode")
     {
-        if (!check_opt_arg(opt, 4)) return false;
-        if (!parse_arg_mode(opt + 5)) return false;
+        if (!check_opt_arg(opt, 6)) return false;
+        if (!parse_arg_mode(opt + 7)) return false;
     }
-    else if (topt == "s")
+    else if (topt.substr(0, 8) == "--escape")
+    {
+        if (!check_opt_arg(opt, 8)) return false;
+        if (!parse_arg_escape(opt + 9)) return false;
+    }
+    else if (topt == "-s")
     {
         if (optmode == 2)
         {
@@ -116,7 +131,7 @@ bool parse_opt(const char* opt)
         }
         optmode = 1;
     }
-    else if (topt == "t")
+    else if (topt == "-t")
     {
         if (optmode == 1)
         {
@@ -125,22 +140,22 @@ bool parse_opt(const char* opt)
         }
         optmode = 2;
     }
-    else if (topt == "r")
+    else if (topt == "-r")
     {
         if (optmode == 2)
         {
             error_opt_conflict("-r", "-t");
             return false;
         }
-        optr = 1;
+        optresponse = 1;
     }
-    else if (topt == "e")
+    else if (topt == "-e")
     {
-        opte = 1;
+        optescape = 2;
     }
-    else if (topt == "n")
+    else if (topt == "-n")
     {
-        optn = 1;
+        optescape = 4;
     }
     else
     {
@@ -174,17 +189,17 @@ bool parse_arg_mode(const char* arg)
     return true;
 }
 
-bool parse_cmd(int argc, char** argv)
+bool parse_arg_escape(const char* arg)
 {
-    if (!parse_exe_name(argv[0]))
-        return false;
-
-    for (int i = 1; i < argc; ++i)
+    std::string targ{arg};
+    if (targ == "never") optescape = 1;
+    else if (targ == "std") optescape = 2;
+    else if (targ == "strict") optescape = 3;
+    else if (targ == "number") optescape = 4;
+    else
     {
-        std::string s{argv[i]};
-        int t = check_opt(argv[i]);
-        if (t == 0) return false;
-        if (!parse_opt(argv[i] + t)) return false;
+        error_arg_bad(arg, "--escape=");
+        return false;
     }
     return true;
 }
@@ -230,12 +245,13 @@ void show_error()
 bool check_opt_value()
 {
     if (optmode == 0) optmode = 1;
+    if (optescape == 0) optescape = 2;
     return true;
 }
 
 int work_mode_s()
 {
-    if (optr == 1) return work_mode_s_with_r();
+    if (optresponse == 1) return work_mode_s_with_r();
     else return work_mode_s_without_r();
 }
 
@@ -248,35 +264,68 @@ int work_mode_s_without_r()
         std::cout << '\"';
         for (char c : s)
         {
-            if (optn)
+            if (optescape == 1)
+            {
+                std::cout << c;
+            }
+            else if (optescape == 2)
+            {
+                if (c == '\'')
+                {
+                    std::cout << "\\\'";
+                }
+                else if (c == '\"')
+                {
+                    std::cout << "\\\"";
+                }
+                else if (c == '\\')
+                {
+                    std::cout << "\\\\";
+                }
+                else if (c == '\0')
+                {
+                    std::cout << "\\0";
+                }
+                else if (std::iscntrl(c))
+                {
+                    std::cout << "\\x" << std::setw(2) << (int(c)&0xff);
+                }
+                else
+                {
+                    std::cout << c;
+                }
+            }
+            else if (optescape == 3)
+            {
+                if (c == '\'')
+                {
+                    std::cout << "\\\'";
+                }
+                else if (c == '\"')
+                {
+                    std::cout << "\\\"";
+                }
+                else if (c == '\\')
+                {
+                    std::cout << "\\\\";
+                }
+                else if (c == '\0')
+                {
+                    std::cout << "\\0";
+                }
+                else if (std::iscntrl(c) || (int(c)&0x80))
+                {
+                    std::cout << "\\x" << std::setw(2) << (int(c)&0xff);
+                }
+                else
+                {
+                    std::cout << c;
+                }
+            }
+            else if (optescape == 4)
             {
                 std::cout << "\\x" << std::setw(2) << (int(c)&0xff);
                 continue;
-            }
-
-            if (c == '\'')
-            {
-                std::cout << "\\\'";
-            }
-            else if (c == '\"')
-            {
-                std::cout << "\\\"";
-            }
-            else if (c == '\\')
-            {
-                std::cout << "\\\\";
-            }
-            else if (c == '\0')
-            {
-                std::cout << "\\0";
-            }
-            else if (std::iscntrl(c))
-            {
-                std::cout << "\\x" << std::setw(2) << (int(c)&0xff);
-            }
-            else
-            {
-                std::cout << c;
             }
         }
         std::cout << "\"\n";
